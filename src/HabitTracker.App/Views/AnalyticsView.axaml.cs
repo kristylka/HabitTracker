7a3a779be2cdef9884e8d68515c2d7c876a3c1d4
kristylka <kristina.cherkasova.2006@gmail.com>
2026-05-20 21:52:47 +0300

    user control аналитики

---
+++ src/HabitTracker.App/Views/AnalyticsView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HabitTracker.App.Views;
+
+public partial class AnalyticsView : UserControl
+{
+    public AnalyticsView()
+    {
+        InitializeComponent();
+    }
+}
