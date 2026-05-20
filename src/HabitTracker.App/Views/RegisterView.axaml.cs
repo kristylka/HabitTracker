bf77653dfde47c3283cd96e22932b8d7dac9ca95
kristylka <kristina.cherkasova.2006@gmail.com>
2026-05-20 20:57:27 +0300

    user control для регистрации

---
+++ src/HabitTracker.App/Views/RegisterView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HabitTracker.App.Views;
+
+public partial class RegisterView : UserControl
+{
+    public RegisterView()
+    {
+        InitializeComponent();
+    }
+}
